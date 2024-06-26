0579868af45339b01c8b9f2b49ee502531436f3d
Martin Lücke <martin.luecke@ed.ac.uk>
2024-06-26 15:52:14 +0200

    WIP: graph expansion
    Signed-off-by: Martin Lücke <martin.luecke@ed.ac.uk>

--- shark_turbine/kernel/wave/wave.py
+++ shark_turbine/kernel/wave/wave.py
@@ -1,3 +1,4 @@
+from math import e
 from typing import Any, Callable, Optional, Type
 import inspect
 import os
@@ -10,6 +11,7 @@ from .constraints import (
     get_grid_shape,
 )
 from .codegen import WaveEmitter
+from .expansion import expand_graph
 from ..lang import Grid
 from ..ops import wave_ops
 from .._support.tracing import (
@@ -89,7 +91,13 @@ class LaunchableWave(Launchable):
         module_op: Optional[Operation] = None,
     ) -> CapturedTrace:
         # Trace the function.
-        trace = self._trace()
+        graph = self._trace()
+
+        # Expansion
+        expand_graph(graph, self.constraints)
+
+        print(f"Expanded:\n{graph.get_root_graph()}")
+        print(f"Expanded Reduce:\n{graph.get_subgraph('region_0')}")
 
         kernel_sig = kernel_codegen.KernelSignature()
         # Fixed values for now, will be determined through constraints
@@ -101,10 +109,10 @@ class LaunchableWave(Launchable):
         exe = dispatch_codegen.StreamExecutable(mb, name=entrypoint_name)
         dispatch_entrypoint = exe.define_entrypoint(entrypoint_name, kernel_sig, grid)
 
-        emitter = WaveEmitter(dispatch_entrypoint, trace)
-        emitter.emit(trace.get_root_graph())
+        emitter = WaveEmitter(dispatch_entrypoint, graph)
+        emitter.emit(graph.get_root_graph())
 
-        return trace
+        return graph
 
     def test_execute(self, args, kwargs):
         # For now only tracing
